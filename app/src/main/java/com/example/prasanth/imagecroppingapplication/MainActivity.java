dab05b85616c0ddd812107629dae49523d9330c0
prashanth1233 <prashanth.chintala@raybiztech.com>
2017-04-19 10:46:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/prasanth/imagecroppingapplication/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.prasanth.imagecroppingapplication;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button selectPhotoBtn;
+    private AlertDialog.Builder alertDialog;
+    private ImageView image;
+    private String imagePath;
+    private Uri selectedImage, imageUrl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image = (ImageView) findViewById(R.id.image);
+        selectPhotoBtn = (Button) findViewById(R.id.selectPhotoBtn);
+        selectPhotoBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        selectPhoto();
+    }
+
+    private void selectPhoto() {
+        final String[] dialogItems = {"Take Photo", "Upload Photo"};
+        alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle("Add Photos");
+        alertDialog.setItems(dialogItems, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int item) {
+                if (dialogItems[item].equals("Take Photo")) {
+                    Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    File file = new File(Environment.getExternalStorageDirectory(), "croppingImages.jpg");
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                    startActivityForResult(intent, 1);
+                } else if (dialogItems[item].equals("Upload Photo")) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 2);
+                }
+            }
+        });
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == 1) {
+                File file = new File(Environment.getExternalStorageDirectory().toString());
+                for (File f : file.listFiles()) {
+                    if (f.getName().equals("croppingImages.jpg")) {
+                        file = f;
+                        imagePath = file.getAbsolutePath();
+                        selectedImage = Uri.fromFile(file);
+                        break;
+                    }
+                }
+
+                BitmapFactory.Options bitmapFactory = new BitmapFactory.Options();
+                Bitmap bitmap = BitmapFactory.decodeFile(imagePath, bitmapFactory);
+                image.setImageBitmap(bitmap);
+                imageCropFunction();
+            } else if (requestCode == 2) {
+
+                selectedImage = data.getData();
+                String[] filePaths = {MediaStore.Images.Media.DATA};
+                Cursor cursor = getContentResolver().query(selectedImage, filePaths, null, null, null);
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePaths[0]);
+                String filePathss = cursor.getString(columnIndex);
+                cursor.close();
+                BitmapFactory.Options bitmapFactoryGallery = new BitmapFactory.Options();
+                Bitmap bitmap = BitmapFactory.decodeFile(filePathss, bitmapFactoryGallery);
+                image.setImageBitmap(bitmap);
+                imageCropFunction();
+            } else if (requestCode == 3) {
+                Bundle extras = data.getExtras();
+                Bitmap thePic = extras.getParcelable("data");
+                image.setImageBitmap(thePic);
+            }
+        }
+    }
+
+    private void imageCropFunction() {
+        Intent viewMediaIntent = new Intent();
+        viewMediaIntent.setAction("com.android.camera.action.CROP");
+        viewMediaIntent.setDataAndType(selectedImage, "image/*");
+        viewMediaIntent.putExtra("crop", "true");
+        viewMediaIntent.putExtra("aspectX", 1);
+        viewMediaIntent.putExtra("aspectY", 1);
+        viewMediaIntent.putExtra("outputX", 256);
+        viewMediaIntent.putExtra("outputY", 256);
+        viewMediaIntent.putExtra("scaleUpIfNeeded", true);
+        viewMediaIntent.putExtra("return-data", true);
+        startActivityForResult(viewMediaIntent, 3);
+    }
+}
+
+
+
